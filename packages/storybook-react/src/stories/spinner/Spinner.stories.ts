0136b4a3846db6415e70d8c26361d1cf6c6da0df
Pablo Coronel <pablo.coronel@garajedeideas.com.ar>
2024-01-15 13:33:54 -0300

    added Spinner component

---
+++ packages/storybook-react/src/stories/spinner/Spinner.stories.ts
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Spinner } from "@cromaui/react";
+
+const meta = {
+  title: "Data Display/Spinner",
+  component: Spinner,
+  tags: ["autodocs"],
+  argTypes: {
+    withLogo: {
+      control: { type: "boolean" },
+      description: "Muestra una variante basada en el logo de Macro.",
+      table: {
+        defaultValue: { summary: false },
+        type: { summary: "boolean" },
+      },
+    },
+    label: {
+      control: { type: "text" },
+      description: "El mensaje de carga que muestra el",
+      table: {
+        defaultValue: { summary: `'Cargando...'` },
+        type: { summary: "string" },
+      },
+    },
+  },
+} satisfies Meta<typeof Spinner>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const SpinnerDefault: Story = {
+  name: "Default",
+  args: {
+    withLogo: false,
+    label: undefined,
+  },
+};
+
+export const SpinnerWithLabel: Story = {
+  name: "Con texto",
+  args: {
+    withLogo: false,
+    label: "Cargando...",
+  },
+};
+
+export const SpinerLogoDefault: Story = {
+  name: "Con logo",
+  args: {
+    withLogo: true,
+    label: undefined,
+  },
+};
+
+export const SpinnerLogoWithLabel: Story = {
+  name: "Con logo y texto",
+  args: {
+    withLogo: true,
+    label: "Cargando...",
+  },
+};
