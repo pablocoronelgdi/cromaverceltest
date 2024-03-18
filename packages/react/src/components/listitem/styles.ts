51b5297a489322aeee95e699b7dcd2b8753bdab3
Pablo Coronel <pablo.coronel@garajedeideas.com.ar>
2024-03-18 17:01:12 -0300

    Fixed minor details & branch conflict

--- packages/react/src/components/listitem/styles.ts
+++ packages/react/src/components/listitem/styles.ts
@@ -15,58 +15,54 @@ import type { ListItemPropsTypes } from './types'
 ============================================= */
 
 export const StyledListItem = styled.div<ListItemPropsTypes>`
-  width: 100%;
-  min-width: 300px;
-  max-width: 1024px;
-  background: ${color.neutral[50]};
   ${FLEX_AROUND}
+  background: ${(props) => props.$disabled ? color.neutral[200] : color.neutral[50]};
+  color: ${(props) => props.$disabled ? color.neutral[400] : color.neutral[700]};
+  width: 100%;
+  max-width: 1024px;
   padding: ${spacings.space12};
   border-bottom: ${borders.br1} ${color.neutral[300]};
-  transition: 0.3s;
-  color: ${color.neutral[700]};
+  transition: all 0.3s ease-in-out;
+  cursor: ${(props) => props.$disabled ? 'no-drop' : 'pointer'};
 
   &:hover {
-    box-shadow: ${elevations.level3};
+    box-shadow: ${(props) => props.$disabled ? 'none' : elevations.level3};
     transition: 0.3s;
   }
-  &:focus {
+  &:focus,
+  &:focus-within,
+  &:focus-visible {
     border: ${borders.br2} ${color.blue.main}
   }
   &.pressed {
-    background: ${color.navy.extraSoft}
-  }
-  &.disabled {   
-    background: ${color.neutral[200]};
-    color: ${color.neutral[400]};
-
-    &:hover {
-    box-shadow: none;
-    transition: 0.3s;
-  }
+    background: ${(props) => props.$disabled ? color.neutral[200] : color.navy.extraSoft};
   }
 `
-export const RightContent = styled.div<ListItemPropsTypes>`
+export const RightContent = styled.div`
   ${FLEX_END}
   margin-left: ${spacings.space16};
   width: 100%;
 `
-export const LeftContent = styled.div<ListItemPropsTypes>`
+export const LeftContent = styled.div`
   ${FLEX_CENTER}
   margin-right: ${spacings.space16};
 `
-export const TextContent = styled.div<ListItemPropsTypes>`
+export const TextContent = styled.div`
   width: 100%;
 
-  h3 {
+  p {
     ${FONT_BODY_MD}
     ${FONT_TYPE_REGULAR}
+    color: ${color.neutral[900]};
     text-align: left;
     width: 100%;
+    margin: 0
   }
-  p {
+  span {
     ${FONT_BODY_SM}
     ${FONT_TYPE_REGULAR}
     text-align: left;
     width: 100%;
+    margin: 0
   }
 `
--- packages/react/src/components/listitem/types.ts
+++ packages/react/src/components/listitem/types.ts
@@ -1,9 +1,8 @@
-import { type ReactNode } from 'react'
+import type { LiHTMLAttributes, ReactNode } from 'react'
 
 export type ListItemPropsTypes = {
-  title: string
-  subtitle?: string
-  contentLeft?: ReactNode | undefined
-  contentRight?: ReactNode | undefined
-  disabled?: boolean
-}
+  $description?: string
+  $contentLeft?: ReactNode | undefined
+  $contentRight?: ReactNode | undefined
+  $disabled?: boolean
+} & LiHTMLAttributes<HTMLElement>
